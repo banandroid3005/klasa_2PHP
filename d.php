7eace62de91aea5cd72b9cbfcf08a46e6c64fc68
banandroid <filip.pakula.29@gmail.com>
2024-09-13 15:39:45 +0200

    zad

---
+++ d.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+<body>
+    <div>Jestem HTML-em</div>
+    <div><?php echo "Jestem PHP-em";?></div>
+</body>
+</html>
---
+++ syntax.php
@@ -0,0 +1,21 @@
+
+<?php
+
+echo 'Tu funkcja echo <br>';
+echo 'Tu ' . 'funkcja '. 'echo'. '<br>';
+echo "Tu funkcja echo <br>";
+echo "Tu " . "funkcja ". "echo". "<br>";
+
+
+print("Zgłasza się funkcja print <br>");
+print("Zgłasza "."się "."funkcja "."print"."<br>");
+
+
+// komentarz jednoliniowy
+# jnj
+/* Komentarz
+wielo
+liniowy*/ 
+?>
+<?="Pierwszy dowolny tekst <br>";?>
+<?="Drugi dowolny tekst <br>";?>
