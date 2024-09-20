c4b25a38b2cfc3a3e72db2e40962996ec4782863
banandroid3005 <filip.pakula.29@gmail.com>
2024-09-20 21:53:51 +0200

    ok

--- wiersze_w_petli.php
+++ wiersze_w_petli.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wiersze w pętli</title>
+    <title>Wiersze w pętli while</title>
     <style>
         table,td{
             background-color: lightgray;
