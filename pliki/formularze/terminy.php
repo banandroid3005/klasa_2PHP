510c70ca41b42d9e722b281f3b5ee03b4ff47604
banandroid <filip.pakula.29@gmail.com>
2025-02-20 15:40:14 +0100

    co

---
+++ pliki/formularze/terminy.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terminy</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&display=swap" rel="stylesheet">
+    <style>
+body{
+    background-color: #fff7d1;
+    padding: 100px;
+    font-family: "Bebas Neue", serif;
+    font-weight: 400;
+    font-style: normal;
+}
+table{
+    background-color: #ffecc8;
+    border-collapse: collapse;
+}
+td{
+    border: solid gray;
+    padding: 10px;
+    text-align: center;
+}
+input{
+    background-color: #ffd09b;
+    border: solid #ffb0b0;
+}
+input[type=checkbox]{
+    accent-color: #ffd09b;
+    outline: #ffb0b0;
+    outline-offset: -1px;
+    transform: scale(2);
+}
+
+        </style>
+</head>
+<body>
+    <form method="post">
+        <table>
+            <caption>Terminy</caption>
+            <?php
+            $plik = true;
+                for($i=1;$i<=5;$i++){
+                    $termin = "termin_".$i;
+                    $month = "Rok_".$i;
+                    $plik = $plik || in_array($termin,($_POST['termin'] ?? [])) && $_POST[$month] ? $_POST[$month] : '';
+                    $checked = (in_array($termin,$_POST['termin'] ?? []) && isset($_POST[$month]) ? 'checked' : '');
+                    ?>
+                    <tr>
+                        <td>
+                    <label for="<?=$termin?>"> Termin <?=$i?></label>
+                </td>
+                <td>
+                    <input type="checkbox" name="termin[]" id="<?=$termin?>" value = "<?=$termin?>" <?=$checked?>>
+                </td>
+                <td>
+                    <input type="month" name="<?=$month?>" id = "termin_miesiaca_"<?=$i?> value="<?=in_array($termin,($_POST['termin'] ?? [])) && $_POST[$month] ? $_POST[$month] : ''?>">
+                </td>
+                </tr>
+                    <?php
+                }
+            ?>
+                    
+                    <tr>
+            <td colspan="3">
+                <input type="submit" value="Zapisz">
+            </td>
+                    </tr>
+        </table>
+        <pre>
+            <?php
+                print_r($_POST);
+            ?>
+        </pre>
+        <?php
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                if($plik){
+                    $file = 'plik.txt';
+                    $filehandle = fopen($file,"a");
+                    $wpisz = date("Y-m-d H: i s") . "\n".print_r($_POST,true);
+                    $filehandle = fwrite($wpisz);
+                    $filehandle=fclose($plik);
+                }
+        }
+        ?>
+    </form>
+</body>
+</html>
