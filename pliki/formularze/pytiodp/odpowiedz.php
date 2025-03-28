65a634e9ac2ddb5302992f49500fc7b16f17a2b0
banandroid3005 <filip.pakula.29@gmail.com>
2025-03-28 13:55:26 +0100

    Add files via upload

--- pliki/formularze/pytiodp/odpowiedz.php
+++ pliki/formularze/pytiodp/odpowiedz.php
@@ -6,29 +6,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Gunjala+Gondi:wght@400..700&display=swap" rel="stylesheet">
+    <title>Odpowiedź</title>
 </head>
 <body>
     <?php
         if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['user_answer'])){
     ?>
     <div class="container">
+        <div class="text">
         <h1>Odpowiedź</h1>
         <p>Trudność: <?=$_SESSION['difficulty']?></p>
         <p>Kategoria <?= $_SESSION['category']?></p>
         <p>Pytanie: <?=$_SESSION['question']?></p>
         <p>Wybrałeś: <?=$_POST['user_answer']?></p>
         <p><?php if($_POST['user_answer'] == $_SESSION['correct_answer']){
-             echo "Poprawna odpowiedź"; 
+             echo "<p class = dobre> Poprawna odpowiedź"; 
             }else{
-                echo "Niepoprawna odpowiedź <br>";
-                echo "Poprawna odpowiedź to: {$_SESSION['correct_answer']}";
+                echo "<p class = zle>Niepoprawna odpowiedź </p>";
+                echo "<p class = zle> Poprawna odpowiedź to: {$_SESSION['correct_answer']}";
             }
             ?></p>
     <?php
         }
         ?>
         <a href="pytanie.php">Kolejne pytanie</a>
+    </div>
         </div>
         <?php
             session_destroy();
--- pliki/formularze/pytiodp/pytanie.php
+++ pliki/formularze/pytiodp/pytanie.php
@@ -6,15 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Gunjala+Gondi:wght@400..700&display=swap" rel="stylesheet">
+    <title>Pytanie</title>
 </head>
 <body>
     <?php
 
         $url = 'https://opentdb.com/api.php?amount=1&category=18&type=multiple';
         $json = file_get_contents($url);
-        $data = json_decode($json,true);
-        if($data){
+        if($json){
+            $data = json_decode($json,true);
             if($data['response_code'] == 0){
                 $difficulty = $data['results'][0]['difficulty'];
                 $category = $data['results'][0]['category'];
@@ -31,8 +34,9 @@
         }
     ?>
     <div class="container">
+        <div class="text">
     <form action="odpowiedz.php" method="post">
-        <h1>Quiz</h1>
+        <h1>Quiz z OpenTDB</h1>
         <p>Trudność: <?=$difficulty?></p>
         <p>Kategoria <?= $category?></p>
         <p>Pytanie: <?=$question?></p>
@@ -40,8 +44,9 @@
             <input type="radio" name="user_answer" id="user_answer_<?=$answer?>" value="<?= htmlspecialchars($answer) ?? ''?>" required>
             <label for="user_answer_<?=$answer?>"><?=$answer?></label><br>
             <?php }?></p>
-        <button type="submit" >Sprwadz odpowiedz</button>
+        <button type="submit" >Sprawdź odpowiedź</button>
     </form>
+        </div>
     </div>
 </body>
 </html>
