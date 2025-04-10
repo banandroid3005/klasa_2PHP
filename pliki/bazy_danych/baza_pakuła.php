1743a76816b31677c8596583f3826891739c1bc2
banandroid <filip.pakula.29@gmail.com>
2025-04-10 15:40:19 +0200

    baz

---
+++ pliki/bazy_danych/baza_pakuła.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Połączenie z bazą</title>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "pakuła";
+$password = "banan123";
+$database = "pakuła";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+  die("Nie połączono: " . $conn->connect_error);
+}
+echo "Udane połączenie";
+?>
+
+</body>
+</html>
